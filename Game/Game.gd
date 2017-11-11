3c836ddb9e3eb65149a79706b57c6b6476b7ea4b
Oindril Dutta <duttaoindril@gmail.com>
2017-11-10 23:18:47 -0800

    made some changes

--- Game/Game.gd
+++ Game/Game.gd
@@ -5,7 +5,13 @@ var state
 
 func _ready():
 	randomize()
+	print(get_node("Layer0Map").get_cell_size())
 	state = {
+		"tileSize": 60,
+		"width": get_viewport().get_rect().size[0],
+		"height": get_viewport().get_rect().size[1],
+		"mapWidth": state["width"]/state["tileSize"],
+		"mapHeight": state["height"]/state["tileSize"],
 		"splash": get_node("Splashscreen"),
 		"bg": get_node("BG"),
 		"score1bg": get_node("Panel1"),
@@ -16,21 +22,22 @@ func _ready():
 		"fx": get_node("FX"),
 		"playerStates": [0, 1],
 		"playerScores": [0, 0],
-		"mapWidth": 1920/60,
-		"mapHeight": 1080/60,
 		"layer0": get_node("Layer0Map")
 	}
 	state["splash"].play("intro")
 	act("show", ["splash"])
-	setUp()
 	set_process_input(true)
 
 func setUp():
-	for i in range(1, 3):
-		var playerI = player.instance()
-		playerI.set_name("Player "+str(i))
-		playerI.set_pos(Vector2(640*i, 980))
-		add_child(playerI)
+#	for i in range(1, 3):
+#		var playerI = player.instance()
+#		playerI.set_name("Player "+str(i))
+#		playerI.set_pos(Vector2(state["width"]/3*i, state["height"]-100))
+#		add_child(playerI)
+	var playerI = player.instance()
+	playerI.set_name("Player 1")
+	playerI.set_pos(Vector2(state["width"]/3, state["height"]-100))
+	add_child(playerI)
 
 func clear():
 	for child in get_children():
@@ -39,28 +46,18 @@ func clear():
 			child.queue_free()
 
 func _input(event):
-	if event.is_action_pressed("action"):
-		if state["splash"].is_hidden():
-			pass
-		elif state["splash"].get_animation() == "intro":
+	if event.is_action_pressed("action") && !state["splash"].is_hidden():
+		if state["splash"].get_animation() == "intro":
 			state["splash"].set_animation("survivor")
 		elif state["splash"].get_animation() == "survivor":
 			state["splash"].set_animation("death")
 		elif state["splash"].get_animation() == "death":
 			act("hide", ["splash"])
 			state["splash"].set_animation("default")
-		elif !state["splash"].is_hidden():
-			pass
+			setUp()
 	if event.is_action_pressed("borderless"):
 		OS.set_borderless_window(!OS.get_borderless_window())
 
 func act(funct, args):
 	for arg in args:
 		state[arg].call(funct)
-#		actDict(state[arg], funct)
-
-func actDict(node, action):
-	if action == "show":
-		node.show()
-	elif action == "hide":
-		node.hide()
