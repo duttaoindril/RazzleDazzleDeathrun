95d03d79e954853900817e2c1dd394b290f63b3c
Oindril Dutta <duttaoindril@gmail.com>
2017-11-20 19:45:42 -0800

    added more saw

--- Projectiles/Saw/Saw.gd
+++ Projectiles/Saw/Saw.gd
@@ -5,28 +5,35 @@ const GRAVITY = .2
 const DAMPEN = 0.8
 const JUMP_MULT = 25
 const MOVE_SPEED = 1
-var state
+var state = {}
 #ON INSTANCE RUN ---------------------------------------------------------------------------------------------
 func _ready():
 	state = {"id":get_name()[get_name().length()-1],
 	"otherId":str(int(!(int(get_name()[get_name().length()-1])-1))+1),
 	"name": get_name(),
-	"grounded":false,
-	"velocity":Vector2(),
 	"facing":Vector2(1,0),
-	"sprite":get_node("PlayerSprite"),
-	"body": get_node("PlayerBody"),
-	"action":"idle"}
+	"velocity":Vector2(),
+	"grounded":false,
+	"action":"idle",
+	"sprite":get_node("SawSprite"),
+	"body": get_node("SawBody"),
+	"core": get_node("SawCore"),
+	"name": get_node("SurvivorName"),
+	"sound": get_node("FX")}
+	state["name"].set_text(get_name())
 	set_fixed_process(true)
+	set_process_input(true)
 #INITIALIZATION ----------------------------------------------------------------------------------------------
 func init(name, set):
 	preset = set
 	set_name(name)
-	set_pos(preset["position"])
-	state["facing"] = preset["facing"]
+	state["position"] = preset["position"]
+	reset()
 	return self
 #RUNS 60HZ ----------------------------------------------------------------------------------------------
 func _fixed_process(delta):
+	if !get_viewport_rect().has_point(get_pos()):
+		reset()
 #LEFT AND RIGHT CONTROLS
 	if Input.is_action_pressed("left"+state["id"]):
 		velocity.x -= MOVE_SPEED 
@@ -48,3 +55,6 @@ func _fixed_process(delta):
 		var n = get_collision_normal()
 		velocity = n.slide(velocity)
 	move(velocity)
+#HELPER FUNCTIONS ----------------------------------------------------------------------------------------------
+func reset():
+	set_pos(state["position"])
