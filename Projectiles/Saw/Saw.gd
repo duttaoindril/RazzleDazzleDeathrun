a64acef05302f200e6d9f213da930ccdf75da0b3
Oindril Dutta <duttaoindril@gmail.com>
2017-11-20 18:25:20 -0800

    Adding in Saw

--- Projectiles/Saw/Saw.gd
+++ Projectiles/Saw/Saw.gd
@@ -1,27 +1,31 @@
 extends KinematicBody2D
-
-#PRE SETUP
+#SETUP/Global/Constants
 const MOVE_SPEED = 1
-var velocity = Vector2()
-var speed = 1
-var gravity = .2
+const GRAVITY = .2
+const DAMPEN = 0.8
+const JUMP_MULT = 25
+const MOVE_SPEED = 1
 var state
-
+#ON INSTANCE RUN ---------------------------------------------------------------------------------------------
 func _ready():
-	state = {
-		"id":get_name()[get_name().length()-1],
-		"otherId":str(int(!(int(get_name()[get_name().length()-1])-1))+1),
-		"name": get_name(),
-		"grounded":false,
-		"velocity":Vector2(),
-		"facing":Vector2(1,0),
-		"sprite":get_node("PlayerSprite"),
-		"body": get_node("PlayerBody"),
-		"action":"idle",
-		}
+	state = {"id":get_name()[get_name().length()-1],
+	"otherId":str(int(!(int(get_name()[get_name().length()-1])-1))+1),
+	"name": get_name(),
+	"grounded":false,
+	"velocity":Vector2(),
+	"facing":Vector2(1,0),
+	"sprite":get_node("PlayerSprite"),
+	"body": get_node("PlayerBody"),
+	"action":"idle"}
 	set_fixed_process(true)
-
-
+#INITIALIZATION ----------------------------------------------------------------------------------------------
+func init(name, set):
+	preset = set
+	set_name(name)
+	set_pos(preset["position"])
+	state["facing"] = preset["facing"]
+	return self
+#RUNS 60HZ ----------------------------------------------------------------------------------------------
 func _fixed_process(delta):
 #LEFT AND RIGHT CONTROLS
 	if Input.is_action_pressed("left"+state["id"]):
